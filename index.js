49f5f23eeab0ea2322917427b33242f721727fa5
kelvinclever <kkelvinnzioka@gmail.com>
2023-07-03 22:37:15 +0300

    made changes

--- index.js
+++ index.js
@@ -26,6 +26,6 @@ app.get("/", (req, res) => {
   res.send("hey this is QCS api");
 });
 
-app.listen(PORT || 5000, () => {
+app.listen(PORT, () => {
   console.log(`Server is up and running on port `);
 });
